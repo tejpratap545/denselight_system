56254be6498040efbb406b51255390519dfbf0db
Anshul Gupta <anshul0365@users.noreply.github.com>
2020-12-23 15:36:24 +0530

    fixed layout and script

--- src/Client/pages/index.vue
+++ src/Client/pages/index.vue
@@ -2,22 +2,13 @@
   <v-container fluid fill-height class="pa-0 align-stretch">
     <v-row>
       <v-col cols="0" sm="7" md="8" lg="9" class="login-bg-img">
-        <!-- <div class="center-brand">
-          <div class="inner-container">
-            <i class="fas fa-fire"></i>
-          </div>
-        </div> -->
       </v-col>
 
-      <v-col cols="12" sm="5" md="4" lg="3" class="light pa-15 pa-sm-10">
-        <h1 class="mb-4 font-weight-light text-center">Denselight E-PMP</h1>
-      </v-col>
-
-      <v-col cols="7" sm="5" md="4" lg="3" class="light pa-5 pa-sm-10">
+      <v-col cols="12" sm="5" md="4" lg="3" class="light pa-5 pa-sm-10">
         <h1 class="mb-4 font-weight-light text-center">Denselight E-PMP</h1>
 
         <h2 class="my-8 font-weight-light">Login</h2>
-        <v-form ref="form" lazy-validation @submit.prevent="formsubmit">
+        <v-form ref="form" lazy-validation @submit.prevent="logIn">
           <v-text-field
             v-model="user.username"
             type="text"
@@ -44,13 +35,15 @@
             label="Password"
             :rules="[(v) => !!v || 'Password is required']"
             clearable
+            type="password"
+            required
           ></v-text-field>
           <v-radio-group v-model="user.typeOfEmployee" row mandatory>
-            <v-radio label="Direct" value="direct"></v-radio>
-            <v-radio label="Indirect" value="indirect"></v-radio>
+            <v-radio label="Direct" value="DIRECT"></v-radio>
+            <v-radio label="Indirect" value="INDIRECT"></v-radio>
           </v-radio-group>
-          <div class="text-right" @click="logIn">
-            <v-btn color="primary" elevation="2"> Login </v-btn>
+          <div class="text-right">
+            <v-btn color="primary" elevation="2" type="submit"> Login </v-btn>
           </div>
           <div class="my-4">
             <p class="font-weight-light">
@@ -61,12 +54,6 @@
       </v-col>
     </v-row>
   </v-container>
-  <!-- <v-row align="center" justify="center">
-    <v-col class="text-center" cols="12">
-      <h1 class="display-1 font-weight-thin mb-4">Hello, Vuetify</h1>
-      <h4 class="subheading">Build your application today!</h4>
-    </v-col>
-  </v-row> -->
 </template>
 
 <script>
@@ -87,16 +74,19 @@ export default {
 
   methods: {
     logIn() {
-      logout(this.$auth, this.$axios)
-
-      signIn(
-        this.$axios,
-        this.$auth,
-        this.$store,
-        this.$router,
-        this.$route.query.redirect || '/',
-        this.user
-      )
+      if(this.$refs.form.validate()){
+        console.log(this.user);
+        logout(this.$auth, this.$axios)
+        signIn(
+          this.$axios,
+          this.$auth,
+          this.$store,
+          this.$router,
+          this.$route.query.redirect || '/',
+          this.user
+        )
+      }
+      
     },
   },
 }
@@ -113,22 +103,6 @@ body {
   background-repeat: no-repeat;
   background-size: cover;
 }
-
-/* .center-brand{
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -webkit-transform: translate(-50%,-50%);
-  -ms-transform: translate(-50%,-50%);
-  transform: translate(-50%,-50%);
-}
-.center-brand .inner-container{
-  background: #fff;
-  width: 120px;
-  height: 120px;
-  border-radius: 50%;
-  position: relative;
-} */
 
 @media (max-width: 600px) {
   .login-bg-img {
