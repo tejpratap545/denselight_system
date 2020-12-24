74014d0d9083326e429b9035b8515608a705159d
Anshul Gupta <anshul0365@users.noreply.github.com>
2020-12-25 03:00:08 +0530

    Department Page created

---
+++ src/Client/pages/home/hr/department.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <div class="px-10 my-5">
+        <v-tabs background-color="transparent" color="#2952A4" centered grow v-model="tabData">
+          <v-tab>Department Details</v-tab>
+          <v-tab>Department Goals and Core Values</v-tab>
+          <v-tab>Subordinate Appraisal</v-tab>
+        </v-tabs>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Department',
+    layout: 'hrDashboard',
+    data(){
+        return{
+            tabData: null,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
