1dc19fb4e7e7fc06d7d962c110dc0c98d1c8abf7
Tejpratap <tejpratapsingh545@outlook.com>
2021-03-09 04:37:45 +0530

    change permission

--- src/Server/backend/Profile/permissions.py
+++ src/Server/backend/Profile/permissions.py
@@ -5,24 +5,24 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.email == request.user.email
+        return obj.email == request.user.email or request.user.role == "Admin"
 
 
 class IsEmployee(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user.role == "Employee"
+        return request.user.role == "Employee" or request.user.role == "Admin"
 
 
 class IsManager(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user.role == "Manager"
+        return request.user.role == "Manager" or request.user.role == "Admin"
 
 
 class IsHr(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user.role == "Hr"
+        return request.user.role == "Hr" or request.user.role == "Admin"
 
 
 class IsHrManager(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user.role == "HRManager"
+        return request.user.role == "HRManager" or request.user.role == "Admin"
