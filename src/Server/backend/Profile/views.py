c3f7fb131ab270ec6b7b57b4dacf9bb73cc1a0a0
Tejpratap <tejpratapsingh545@outlook.com>
2021-05-24 21:16:14 +0530

    report

--- src/Server/backend/Profile/views.py
+++ src/Server/backend/Profile/views.py
@@ -8,7 +8,6 @@ from django.shortcuts import get_list_or_404, render
 from django.utils import timezone
 from rest_framework import status
 from rest_framework.decorators import api_view
-
 # Create your views here.
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -228,7 +227,7 @@ def download_report(request):
 
     data = User_Appraisal_List.objects.prefetch_related(
         "employee__department", "employee"
-    ).filter(is_closed=False)
+    ).exclude(employee__is_active=False)
 
     if appraisal_list is not None or appraisal_list is not []:
         data = data.filter(overall_appraisal_id__in=appraisal_list)
